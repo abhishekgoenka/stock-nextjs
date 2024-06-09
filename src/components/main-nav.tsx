13d78c024d3d81c1a79c804cef7dfdfd0c0b5119
Abhishek Goenka <govindkaran@gmail.com>
2024-06-09 08:28:51 -0500

    Theme added

---
+++ src/components/main-nav.tsx
@@ -0,0 +1,45 @@
+
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+
+export type NavItem = {
+  title: string
+  href?: string
+  disabled?: boolean
+  external?: boolean
+}
+
+type MainNavProps = {
+  items?: NavItem[]
+}
+
+export function MainNav({ items }: MainNavProps) {
+  return (
+    <div className="flex gap-6 md:gap-10">
+      <a href="/" className="flex items-center space-x-2">
+        <Icons.activity className="h-6 w-6" />
+        <span className="inline-block font-bold">{siteConfig.name}</span>
+      </a>
+      {items?.length ? (
+        <nav className="flex gap-6">
+          {items?.map(
+            (item, index) =>
+              item.href && (
+                <a
+                  key={index}
+                  href={item.href}
+                  className={cn(
+                    "flex items-center text-sm font-medium text-muted-foreground",
+                    item.disabled && "cursor-not-allowed opacity-80"
+                  )}
+                >
+                  {item.title}
+                </a>
+              )
+          )}
+        </nav>
+      ) : null}
+    </div>
+  )
+}
---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "StockSync",
+  description:
+    "stock management app",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
