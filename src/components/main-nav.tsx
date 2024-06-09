e8aa890478cd564cddd86872087ca8292d7321c0
Abhishek Goenka <govindkaran@gmail.com>
2024-06-09 12:38:31 -0500

    Menu updated

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -11,7 +11,36 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
+import React from "react";
+import Link from "next/link";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Buy Stock",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "Allows users to initiate and manage stock purchasing activities.",
+  },
+  {
+    title: "Buy Mutual Fund",
+    href: "/docs/primitives/hover-card",
+    description:
+      "This feature simplifies the process of purchasing mutual funds",
+  },
+  {
+    title: "Fund Transfer",
+    href: "/docs/primitives/progress",
+    description:
+      "Enables seamless movement of funds between accounts.",
+  },
+  {
+    title: "Dividend",
+    href: "/docs/primitives/scroll-area",
+    description: "Records dividends received from investments",
+  }
+];
 
 export type NavItem = {
   title: string;
@@ -31,36 +60,115 @@ export function MainNav({ items }: MainNavProps) {
         <Icons.activity className="h-6 w-6" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </a>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
 
-      {items?.length ? (
-        <nav className="flex gap-6">
-          {items?.map(
-            (item, index) =>
-              item.href && (
-                <a
-                  key={index}
-                  href={item.href}
-                  className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
-                    item.disabled && "cursor-not-allowed opacity-80"
-                  )}
+      <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Dashboard
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Profile Management</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <Icons.activity className="h-6 w-6" />
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      StockSync
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Simplify Your Inventory and Order Management
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Add Company Information">
+              This menu adds information about the companies
+              </ListItem>
+              <ListItem
+                href="/docs/installation"
+                title="Add Mutual Fund Information"
+              >
+                This menu is designed add detailed information about MF.
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Inventory Management</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
                 >
-                  {item.title}
-                </a>
-              )
-          )}
-        </nav>
-      ) : null}
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Reports
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Charts
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Setting
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+
     </div>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
+
