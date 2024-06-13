2e648ee0b34da2853f200a8fedfe2b9056a8b6d7
Abhishek Goenka <govindkaran@gmail.com>
2024-06-13 18:29:54 -0500

    Number format

--- src/components/companies/columns.tsx
+++ src/components/companies/columns.tsx
@@ -5,6 +5,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "../ui/data-table/data-table-column-header";
 import { DataTableRowActions } from "./data-table-row-actions";
 import Company from "@/models/company.model";
+import { NumericFormat } from "react-number-format";
 
 export const Columns: ColumnDef<Company>[] = [
   {
@@ -80,68 +81,21 @@ export const Columns: ColumnDef<Company>[] = [
     ),
     cell: ({ row }) => {
       return (
-        <div className="flex space-x-2">
-          <span className="max-w-[200px] truncate font-medium">
-            {row.getValue("currentPrice")}
+        <div className="flex space-x-2 w-20 flex-row-reverse">
+          <span className="font-medium text-center">
+            <NumericFormat
+              displayType="text"
+              decimalScale={2}
+              fixedDecimalScale
+              thousandsGroupStyle="lakh"
+              thousandSeparator=","
+              value={row.getValue("currentPrice")}
+            />
           </span>
         </div>
       );
     },
   },
-  // {
-  //   accessorKey: "status",
-  //   header: ({ column }) => (
-  //     <DataTableColumnHeader column={column} title="Status" />
-  //   ),
-  //   cell: ({ row }) => {
-  //     const status = statuses.find(
-  //       status => status.value === row.getValue("status"),
-  //     );
-
-  //     if (!status) {
-  //       return null;
-  //     }
-
-  //     return (
-  //       <div className="flex w-[100px] items-center">
-  //         {status.icon && (
-  //           <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-  //         )}
-  //         <span>{status.label}</span>
-  //       </div>
-  //     );
-  //   },
-  //   filterFn: (row, id, value) => {
-  //     return value.includes(row.getValue(id));
-  //   },
-  // },
-  // {
-  //   accessorKey: "priority",
-  //   header: ({ column }) => (
-  //     <DataTableColumnHeader column={column} title="Priority" />
-  //   ),
-  //   cell: ({ row }) => {
-  //     const priority = priorities.find(
-  //       priority => priority.value === row.getValue("priority"),
-  //     );
-
-  //     if (!priority) {
-  //       return null;
-  //     }
-
-  //     return (
-  //       <div className="flex items-center">
-  //         {priority.icon && (
-  //           <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-  //         )}
-  //         <span>{priority.label}</span>
-  //       </div>
-  //     );
-  //   },
-  //   filterFn: (row, id, value) => {
-  //     return value.includes(row.getValue(id));
-  //   },
-  // },
   {
     id: "actions",
     cell: ({ row }) => <DataTableRowActions row={row} />,
