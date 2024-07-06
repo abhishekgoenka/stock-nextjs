ca5d9286048be60a3da27163c58f61e6a28e2784
Abhishek Goenka <govindkaran@gmail.com>
2024-07-06 06:52:25 -0500

    Added delete confirmation test

---
+++ src/components/shared/delete-confirmation.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import { describe, it, expect, afterEach, vi } from "vitest";
+import { DeleteConfirmation } from "./delete-confirmation";
+import userEvent from "@testing-library/user-event";
+
+describe("NumberFormater", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("should show delete confirmation", () => {
+    const mockOnCancel = vi.fn();
+    const mockOnContinue = vi.fn();
+    render(<DeleteConfirmation open={true} onCancel={mockOnCancel} onContinue={mockOnContinue} />);
+    expect(screen.getByRole("heading").textContent).toBe("Are you absolutely sure?");
+  });
+
+  it("should close dialog on Cancel", async () => {
+    const user = userEvent.setup();
+    const mockOnCancel = vi.fn();
+    const mockOnContinue = vi.fn();
+
+    render(<DeleteConfirmation open={true} onCancel={mockOnCancel} onContinue={mockOnContinue} />);
+    await user.click(screen.getByRole("button", { name: /Cancel/i }));
+    expect(mockOnCancel).toBeCalledTimes(1);
+    expect(mockOnContinue).toBeCalledTimes(0);
+  });
+
+  it("should close dialog on Continue", async () => {
+    const user = userEvent.setup();
+    const mockOnCancel = vi.fn();
+    const mockOnContinue = vi.fn();
+
+    render(<DeleteConfirmation open={true} onCancel={mockOnCancel} onContinue={mockOnContinue} />);
+    await user.click(screen.getByRole("button", { name: /Continue/i }));
+    expect(mockOnCancel).toBeCalledTimes(0);
+    expect(mockOnContinue).toBeCalledTimes(1);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,8 @@ export default defineConfig({
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: "jsdom",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
 });
