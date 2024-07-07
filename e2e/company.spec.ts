8ffc50e763ce530baf9f12dd3949130f080d6d4f
Abhishek Goenka <govindkaran@gmail.com>
2024-07-07 15:38:15 -0500

    Update company.spec.ts

--- e2e/company.spec.ts
+++ e2e/company.spec.ts
@@ -19,27 +19,37 @@ test("add new company", async ({ page }) => {
   await page.getByText("Company Information").click();
 
   // should have add company button
-  await expect(page.getByRole("button", { name: "Add Company" })).toBeVisible();
+  // await expect(page.getByRole("button", { name: "Add Company" })).toBeVisible();
 
-  page.getByRole("button", { name: "Add Company" }).click();
-  await expect(page.getByRole("heading", { name: "Add Company" })).toBeVisible();
+  // page.getByRole("button", { name: "Add Company" }).click();
+  // await expect(page.getByRole("heading", { name: "Add Company" })).toBeVisible();
 
   // add new company
-  await page.getByPlaceholder("Company name").fill("Dummy company");
-  await page.getByRole("combobox").first().click();
-  await page.keyboard.down("ArrowDown");
-  await page.keyboard.down("ArrowDown");
-  await page.keyboard.press("Enter");
+  const companyName = "Dummy company sdsla";
+  // await page.getByPlaceholder("Company name").fill("Dummy company sdsla");
+  // await page.getByLabel("Sector").click();
+  // await page.keyboard.down("ArrowDown");
+  // await page.keyboard.down("ArrowDown");
+  // await page.keyboard.press("Enter");
 
-  await page.getByRole("combobox").nth(1).click();
-  await page.keyboard.down("ArrowDown");
-  await page.keyboard.down("ArrowDown");
-  await page.keyboard.press("Enter");
+  // await page.getByLabel("Type").click();
+  // await page.keyboard.down("ArrowDown");
+  // await page.keyboard.down("ArrowDown");
+  // await page.keyboard.press("Enter");
 
-  await page.getByRole("combobox").nth(2).click();
-  await page.keyboard.down("ArrowDown");
-  await page.keyboard.press("Enter");
-  await page.getByPlaceholder("Symbol").fill("Test Symbol");
-  await page.getByPlaceholder("URL").fill("http:\\www.test.com");
-  await page.getByRole("button", { name: "Save" }).click();
+  // await page.getByLabel("Exchange").click();
+  // await page.keyboard.down("ArrowDown");
+  // await page.keyboard.press("Enter");
+  // await page.getByPlaceholder("Symbol").fill("Test Symbol");
+  // await page.getByPlaceholder("URL").fill("http:\\www.test.com");
+  // await page.getByRole("button", { name: "Save" }).click();
+
+  // await expect(page.getByText("Your changes have been saved successfully", { exact: true })).toBeVisible();
+
+  //validate the new company
+  await page.getByPlaceholder("Filter companies...").pressSequentially(companyName);
+  await expect(page.getByRole("cell", { name: companyName })).toBeVisible();
+  await expect(page.getByRole("cell", { name: "Commodity Chemicals" })).toBeVisible();
+  await expect(page.getByRole("cell", { name: "Smallcap" })).toBeVisible();
+  await expect(page.getByRole("cell", { name: "NASDAQ" })).toBeVisible();
 });
