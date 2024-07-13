05ed389a5091e7b0b56a6b828a2fd9a95b478cd3
Abhishek Goenka <govindkaran@gmail.com>
2024-07-13 17:28:26 -0500

    Fixed flaky tests

--- e2e/002-company.spec.ts
+++ e2e/002-company.spec.ts
@@ -23,22 +23,9 @@ test("add new company", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "Add Company" })).toBeVisible();
 
   // add new company
-
   await page.getByLabel("Company").pressSequentially(companyName);
-  // await page.getByLabel("Sector").click();
-  // await page.keyboard.down("ArrowDown");
-  // await page.keyboard.down("ArrowDown");
-  // await page.keyboard.press("Enter");
-
   await selectCombo(page, "Sector", "Retailing");
-  await page.getByLabel("Type").click();
-  await page.keyboard.down("ArrowDown");
-  await page.keyboard.down("ArrowDown");
-  await page.keyboard.press("Enter");
-
-  // await page.getByLabel("Exchange").click();
-  // await page.keyboard.down("ArrowDown");
-  // await page.keyboard.press("Enter");
+  await selectCombo(page, "Type", "Smallcap");
   await selectCombo(page, "Exchange", "NSE");
   await page.getByPlaceholder("Symbol").fill("Test Symbol");
   await page.getByPlaceholder("URL").fill("http:\\www.test.com");
@@ -46,10 +33,9 @@ test("add new company", async ({ page }) => {
   await page.waitForTimeout(1000);
 
   await expect(page.getByText("Your changes have been saved successfully", { exact: true })).toBeVisible();
-  await page.waitForTimeout(1000);
+});
 
-  //validate the new company
-  await page.reload();
+test("filter company", async ({ page }) => {
   await page.getByPlaceholder("Filter companies...").pressSequentially(companyName, { delay: 500 });
   await page.getByRole("button", { name: "Type" }).first().focus();
   await expect(page.getByRole("cell", { name: companyName })).toBeVisible();
