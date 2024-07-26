c9ca9eb978ef77d3847afd7fde18410780811b7c
Abhishek Goenka <govindkaran@gmail.com>
2024-07-26 07:00:22 -0500

    Added setting e2e

---
+++ e2e/009-settings.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  // Runs before each test and signs in each page.
+  await page.goto("http://localhost:3001/");
+  await page.getByRole("link", { name: "Setting" }).click();
+});
+
+test("has title", async ({ page }) => {
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/StockSync : Setting/);
+});
+
+test("validate stock sync", async ({ page }) => {
+  await page.getByRole("button", { name: "Sync Stock Price" }).click();
+  await expect(async () => {
+    await expect(page.getByText("Error: Company TestSymbol has")).toBeVisible();
+  }).toPass();
+});
+
+test("validate mutal fund sync", async ({ page }) => {
+  await page.getByRole("button", { name: "Sync Mutual Fund Price" }).click();
+  await expect(async () => {
+    await expect(page.getByText("Error: Fund test mutual fund")).toBeVisible();
+  }).toPass();
+});
